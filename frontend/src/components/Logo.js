f1ab2398074b9b7c109b68bc6be3afd717827ff3
emeorg <michelle.ramirez@mail.udp.cl>
2026-05-07 18:26:11 -0400

    feat: modularización del header e integración de tailwind css

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoaicc from '../img/aicc.png';
+
+export const Logo = () => {
+  return (
+    <Link className="flex items-center" to={'/home'}>
+      <img src={logoaicc} alt='AICC' className="w-[120px] h-auto object-contain" />
+    </Link>
+  );
+};
